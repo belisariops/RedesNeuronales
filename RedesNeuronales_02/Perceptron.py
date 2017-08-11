720ec3a6a596c2f00ed19c3925d55f9923aeab4f
Belisario Panay <belisariops@gmail.com>
2017-08-10 23:36:08 -0400

    Perceptron can now received multiple inputs

--- RedesNeuronales_02/Perceptron.py
+++ RedesNeuronales_02/Perceptron.py
@@ -1,31 +1,47 @@
+
+
 class Perceptron:
+    """the Perceptron is an artificial neuron using the Heaviside step function as the activation function.
+    Attributes:
+        inputs (list(float)): values recived by the perceptron.
+        C (float): Constant value to drecrease or increase a weight of the perceptron proportional to the input, during the training phase.
+        weigths (list(float)): weights of the inputs recieved by the perceptron.
+        bias (float).
+
+    """
     def __init__(self,c):
         self.C  = c
 
-    def setWeights(self,w1,w2):
-        self.w1 = w1
-        self.w2 = w2
+    def setC(self,c):
+        self.C  = c
 
-    def setInputs(self,x1,x2):
-        self.x1 = x1
-        self.x2 = x2
+    def setWeights(self,*weights):
+        self.weights = list(weights)
+
+
+    def setInputs(self,*inputs):
+        self.inputs = list(inputs)
 
     def setBias(self,bias):
         self.bias = bias
 
+
     def getOutput(self):
-        return (self.x1*self.w1 + self.x2*self.w2 + self.bias > 0)
+        output = 0
+        for i in range(len(self.inputs)):
+            output = output + self.inputs[i]*self.weights[i]
+        return output + self.bias > 0
 
     def decreaseWeight(self):
-        self.w1 = self.w1 - self.C*self.x1
-        self.w2 = self.w2 - self.C*self.x2
+        for i in range(len(self.weights)):
+            self.weights[i] = self.weights[i] - self.C*self.inputs[i]
 
     def increaseWeight(self):
-        self.w1 = self.w1 + self.C * self.x1
-        self.w2 = self.w2 + self.C * self.x2
+        for i in range(len(self.weights)):
+            self.weights[i] = self.weights[i] + self.C*self.inputs[i]
 
-    def train(self,x,y,output):
-        self.setInputs(x,y)
+    def train(self,*inputs,output):
+        self.setInputs(*inputs)
         if not output and self.getOutput():
             self.decreaseWeight()
 
---
+++ RedesNeuronales_03/OrPerceptron.py
@@ -0,0 +1,12 @@
+from RedesNeuronales_03.Perceptron import Perceptron
+
+
+class OrPerceptron:
+    def __init__(self,x1,x2):
+        self.perceptron = Perceptron()
+        self.perceptron.setInputs(x1, x2)
+        self.perceptron.setWeights(1, 1)
+        self.perceptron.setBias(0)
+
+    def getOutput(self):
+        return self.perceptron.getOutput()
